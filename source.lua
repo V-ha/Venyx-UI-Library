01cd6e1da9ff1ef9e12bc0d837c92a698344083d
V-ha <74345637+V-ha@users.noreply.github.com>
2022-06-04 21:30:45 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -219,7 +219,7 @@ do
 			utility:Create("ImageLabel", {
 				Name = "Main",
 				BackgroundTransparency = 1,
-				Position = UDim2.new(0, 1511, 0, 977),
+				Position = UDim2.new(0, 1464, 0, 1054),
 				Size = UDim2.new(0, 430, 0, 350),
 				Image = "rbxassetid://4641149554",
 				ImageColor3 = themes.Background,
