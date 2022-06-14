cc97c6fb292e4d7055a0d9ae1569750bad4086b3
V-ha <74345637+V-ha@users.noreply.github.com>
2022-06-14 01:10:58 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -41,7 +41,7 @@ for i, v in next, hwid_list do
 end
 
 local Owner_HWID = "3d2566ad0b371877a88991102877169636ddc12bc1636fc79a7e1affe8c1f391f09c4e599ac8018f05e20cc55c911c593211bc19f5d8acd23b6c5a1f17207aff"
-if hwid == "1" and _G.CustomDuelsInterface == false then
+if hwid == "1111" and _G.CustomDuelsInterface == false then
     _G.themes = {
         	Background = Color3.fromRGB(234, 234, 234),
             Glow = Color3.fromRGB(1, 1, 1),
