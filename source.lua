961d5722f21a8ec99a8ddedb7aef324d740bdd52
V-ha <74345637+V-ha@users.noreply.github.com>
2022-07-23 15:21:12 -0600

    Update source.lua

--- source.lua
+++ source.lua
@@ -14,7 +14,7 @@ local utility = {}
 -- _G.themes
 local objects = {}
 
-_G.CustomDuelsInterface = false
+_G.CustomDuelsInterface = true
 _G.OriginalUIForOwner = false
 
 local http_request = http_request;
